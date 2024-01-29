d5a1ac77dab3d539cf5347dc98fb7aafb676a4dc
kimminam <sdh220405@sdh.hs.kr>
2024-01-30 01:37:04 +0900

    import css and report component

--- fuas/src/app/reportpage/page.js
+++ fuas/src/app/reportpage/page.js
@@ -1,11 +1,15 @@
 "use client";
 import styles from "../page.module.css";
+import '../globals.css'
 import ActionBar from "../components/actionBar";
+import Report from '../components/report'
+import '../static/report.css'
 
 export default function reportPage() {
   return (
     <div className={styles.main}>
       <ActionBar pageTitle="제보방" backUrl="/studentpage" />
+      <Report/>
     </div>
   );
 }
