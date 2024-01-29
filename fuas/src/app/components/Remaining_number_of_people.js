81ad1df4709854c44d6a03a530f72ef80b3380c7
kimminam <sdh220405@sdh.hs.kr>
2024-01-30 01:35:19 +0900

    clean line

--- fuas/src/app/components/Remaining_number_of_people.js
+++ fuas/src/app/components/Remaining_number_of_people.js
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
@@ -30,20 +29,15 @@ function Remaining_number_of_people() {
     );
   }
 
-  const uuids = remaingNumber.map(() => uuidv4());
+  const uuids = remainingNumber.map(() => uuidv4());
 
   return (
     <div className="remaining_number_of_people">
       <div className="member_wrap">
         <p>남은인원</p>
-        {remaingNumber.map((person, index) => (
+        {remainingNumber.map((person, index) => (
           <p key={uuids[index]}>{20 - person.total_count}명</p>
         ))}
-      </div>
-      <div className="move_to_section_wrap">
-        <button className="move_to_section">공지방</button>
-        <button className="move_to_section">제보방</button>
-        <button className="move_to_section">인증방</button>
       </div>
     </div>
   );
