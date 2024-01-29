e7d140a5db664b94c0813d9c70c5310266db2ad2
kimminam <sdh220405@sdh.hs.kr>
2024-01-30 01:35:54 +0900

    add markUp and delete placehold

--- fuas/src/app/components/report.js
+++ fuas/src/app/components/report.js
@@ -1,17 +1,21 @@
 "use client";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
 
 function report() {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
     <div className="report_container">
       <div className="report_wrap">
         <form className="report_form">
           <div className="info">
             <p>제보자</p>
-            <input type="text" placeholder="Ex) 김OO" />
+            <input type="text" />
           </div>
           <div className="info">
             <p>제보할 사람</p>
-            <input type="text" placeholder="Ex) 이OO 혹은 복수 일 경우 김OO, 홍OO..." />
+            <input type="text"/>
           </div>
           <div className="info">
             <p>제목</p>
@@ -19,8 +23,18 @@ function report() {
           </div>
           <div className="info">
             <p>카테고리</p>
-            <input type="text" placeholder="Ex) 김OO" />
+            <input list="category" />
+            <datalist id="category">
+              <option value="음식물 반입 및 취식"/>
+              <option value="소음공해"/>
+              <option value="방해"/>
+            </datalist>
           </div>
+          <div className="info">
+            <p>내용</p>
+            <input type="text" />
+          </div>
+          <button type="button" className="submit" onClick={()=> router.push("/studentpage")}>제보하기</button>
         </form>
       </div>
     </div>
