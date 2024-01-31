60f35e2fbdeb62a2ccd307a6ebb41537ef3771dd
kimminam <bluemind8520@gmail.com>
2024-01-31 20:46:07 +0900

    fix req and add quary body

--- fuas/src/app/components/report.js
+++ fuas/src/app/components/report.js
@@ -2,25 +2,26 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
-function report() {
-  const [id, setId] = useState("");
+function Report() {
+  const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
   const [reporter, setReporter] = useState("");
   const [criminal, setCriminal] = useState("");
   const [report, setReport] = useState("");
-  const [created_at, setCreated_at] = useState("");
   const router = useRouter();
 
-  const handleSubmit = async (e)=> {
-    e.preventDefalut();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const currentTimestamp = Math.floor(new Date().getTime() / 1000); 
+const formattedTimestamp = new Date(currentTimestamp * 1000).toISOString().slice(0, 19).replace("T", " "); 
     const formData = {
-      id: id+1,
+      title : title,
       category: category,
       reporter: reporter,
       criminal: criminal,
       report: report,
-      created_at: created_at,
+      created_at: formattedTimestamp,
     };
 
     try {
@@ -34,7 +35,7 @@ function report() {
 
       if (response.ok) {
         console.log("Data successfully sent to the server");
-        router.push("/studentpage")
+        router.push("/studentpage");
       } else {
         console.error("Failed to send data to the server");
       }
@@ -42,25 +43,26 @@ function report() {
       console.error("Error sending data to the server:", error);
     }
   };
+
   return (
-    <div className="report_container">
+<div className="report_container">
       <div className="report_wrap">
-        <form className="report_form">
+        <form className="report_form" onSubmit={handleSubmit}>
           <div className="info">
             <p>제보자</p>
-            <input type="text" />
+            <input type="text" onChange={(e) => setReporter(e.target.value)}/>
           </div>
           <div className="info">
             <p>제보할 사람</p>
-            <input type="text"/>
+            <input type="text" onChange={(e) => setCriminal(e.target.value)}/>
           </div>
           <div className="info">
             <p>제목</p>
-            <input type="text" />
+            <input type="text" onChange={(e) => setTitle(e.target.value)}/>
           </div>
           <div className="info">
             <p>카테고리</p>
-            <input list="category" />
+            <input list="category" onChange={(e) => setCategory(e.target.value)}/>
             <datalist id="category">
               <option value="음식물 반입 및 취식"/>
               <option value="소음공해"/>
@@ -69,14 +71,15 @@ function report() {
           </div>
           <div className="info">
             <p>내용</p>
-            <input type="text" />
+            <input type="text" onChange={(e) => setReport(e.target.value)}/>
           </div>
-          <button type="submit" className="submit">제보하기</button>
+          <button type="submit" className="submit">
+            제보하기
+          </button>
         </form>
       </div>
     </div>
   );
-  }
+}
 
-
-export default report;
+export default Report;
